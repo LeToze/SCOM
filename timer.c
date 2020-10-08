74d791ff7d1f55a389e744aa939c697aedda8fce
LeToze <50966869+LeToze@users.noreply.github.com>
2020-10-08 19:57:55 +0100

    Timer

--- timer.c
+++ timer.c
@@ -13,25 +13,32 @@ gettimeofday(&start,NULL);
    printf("%ld\n", start.tv_sec);
 }*/
 
-#include <time.h>
-#include <sys/time.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "math.h"
 int main() {
-  struct timeval tv;
-  time_t t;
-  struct tm *info;
-  char buffer[64];
- 
-  gettimeofday(&tv, NULL);
-  t = tv.tv_sec;
 
-  info = localtime(&t);
-  printf("%s",asctime (info));
-  strftime (buffer, sizeof buffer, "Today is %A, %B %d.\n", info);
-  printf("%s",buffer);
-  strftime (buffer, sizeof buffer, "The time is %I:%M %p.\n", info);
-  printf("%s",buffer);
+struct timeval start, end;
+float timer_1;
 
+gettimeofday(&start, NULL);
+
+system("wget http://35.244.95.95:80/cgi-bin/test.cgi --post-data 'n=3' -q");
+
+gettimeofday(&end, NULL);
+  
+   //http://35.244.95.95/cgi-bin/test.cgi
+ timer_1 = ((end.tv_sec * 1E6 + end.tv_usec ) - (start.tv_sec * 1E6 + start.tv_usec));
+
+printf("\n");
+printf("Nice cock Bruno\n\n");
+ printf("Time taken to access CGI with a POST send is : %f seconds\n\n",
+    timer_1*1E-6);
+
+printf("Really, Very Nice cock, Very neat\n\n");
   return 0;
 }
+
